b8927100ceb80abc0fb4a69be48150bacd4ce2f3
Evghenii <egaburov@nvidia.com>
2020-12-02 09:57:43 -0800

    complete ch8

--- rust_book/ch8/src/main.rs
+++ rust_book/ch8/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut v: Vec<i32> = Vec::new();
 
@@ -51,7 +53,7 @@ fn main() {
     println!("s3 is {}", s3);
 
 
-//    let hello = "helloworld";
+    //    let hello = "helloworld";
     let hello = "здраствуйте";
     let s = &hello[0..4];
     println!("s is {}" ,s);
@@ -61,4 +63,69 @@ fn main() {
     for (i,c) in hello.bytes().enumerate() {
         println!("{}: {}", i, c);
     }
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("scores is {:#?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let scores: HashMap<_, _> =
+        teams.into_iter().zip(initial_scores.into_iter()).collect();
+    println!("scores is {:#?}", scores);
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    println!("map is {:#?}", map);
+    // field_name and field_value are invalid at this point, try using them and
+    //println!("{:?}", field_name);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    println!("score is {:?}" ,score);
+    let score = scores.get("Green");
+    println!("score is {:?}" ,score);
+    let score = scores.get("Yellow");
+    println!("score is {:?}" ,score);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    let mut scores = scores;
+    scores.insert(String::from("Blue"), 25);
+    println!("scores is {:#?}", scores);
+
+    let score = scores.entry(String::from("Blue")).or_insert(50);
+    println!("score is {}", score);
+    let score = scores.entry(String::from("Green")).or_insert(70);
+    println!("score is {}", score);
+    println!("scores is {:#?}", scores);
+
+    let text = "hello world wonderful world";
+    println!("text is {}", text);
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+    let mut map = HashMap::new();
+
+    for (i, word) in text.split_whitespace().enumerate() {
+         map.entry(word).or_insert(i);
+    }
+
+    println!("{:?}", map)
+
 }
