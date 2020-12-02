1e6f1b28dd9908abfd28fe1ae9d25cbf473e7f6f
Evghenii <egaburov@nvidia.com>
2020-12-01 16:23:07 -0800

    fix

--- rust_book/ch2/guessing_game/src/main.rs
+++ rust_book/ch2/guessing_game/src/main.rs
@@ -1,11 +1,11 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1,101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
     loop {
