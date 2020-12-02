374b4caf7690a07f27501c3216d960a1f6a9e468
Evghenii <egaburov@nvidia.com>
2020-12-02 07:09:40 -0800

    complete ch7

--- rust_book/ch7/restaurant/src/lib.rs
+++ rust_book/ch7/restaurant/src/lib.rs
@@ -6,20 +6,20 @@ mod tests {
     }
 }
 
-fn serve_order() {}
-
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {
-            super::super::serve_order();
-        }
-    }
-}
+mod front_of_house;
+pub use front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     // absolut path
     crate::front_of_house::hosting::add_to_waitlist();
 
-    // relative path
-    front_of_house::hosting::add_to_waitlist();
+    // relative path, idiomatic use
+    hosting::add_to_waitlist();
+}
+
+use rand::Rng;
+
+pub fn do_something() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret_number= {}", secret_number);
 }
