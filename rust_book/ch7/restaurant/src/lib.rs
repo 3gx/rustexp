236034e232b5e4b392571fbe99d1e14aa4d79c7f
Evghenii <egaburov@nvidia.com>
2020-12-02 06:50:40 -0800

    update ch7

--- rust_book/ch7/restaurant/src/lib.rs
+++ rust_book/ch7/restaurant/src/lib.rs
@@ -5,3 +5,21 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+fn serve_order() {}
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {
+            super::super::serve_order();
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // absolut path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    front_of_house::hosting::add_to_waitlist();
+}
