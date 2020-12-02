69e27c6b372255258afb92293820531639aaa0ae
Evghenii <egaburov@nvidia.com>
2020-12-02 09:05:13 -0800

    update ch8

--- rust_book/ch8/src/main.rs
+++ rust_book/ch8/src/main.rs
@@ -31,4 +31,34 @@ fn main() {
     }
     println!("v is now {:?}", v);
 
+    let s: String;
+    let data = "initial contents";
+    s = data.to_string();
+    println!("s is {}", s);
+
+    let s = String::from("initial contents 2");
+    println!("s is {}", s);
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("s is {}", s);
+
+    s.push('l');
+    println!("s is {}", s);
+
+    let s2 = String::from(" oops");
+    let s3 = s + &s2;
+    println!("s3 is {}", s3);
+
+
+//    let hello = "helloworld";
+    let hello = "здраствуйте";
+    let s = &hello[0..4];
+    println!("s is {}" ,s);
+    for (i,c) in hello.chars().enumerate() {
+        println!("{}: {}", i, c);
+    }
+    for (i,c) in hello.bytes().enumerate() {
+        println!("{}: {}", i, c);
+    }
 }
