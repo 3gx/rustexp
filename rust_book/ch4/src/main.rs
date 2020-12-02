10be1bcb8662d91b3c978d2953b382b3b66416ea
Evghenii <egaburov@nvidia.com>
2020-12-01 17:27:25 -0800

    update ch4

--- rust_book/ch4/src/main.rs
+++ rust_book/ch4/src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
     println!("Hello, world!");
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1= {}", s1);
+    println!("s2= {}", s2);
+
+    let mut s = String::from("hello");
+    {
+        let r1 = &mut s;
+        println !("r1= {}", r1);
+    }
+    let r2 = &mut s;
+    println!("r2= {}", r2);
+    let r3= &s;
+    println!("r3= {}", r3);
+
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("hello= {}", hello);
+    println!("world= {}", world);
+
+    let mut s = String::from("hello world");
+    s.push_str(" yay!");
+    let word = first_word(&s);
+    fn first_word(s: &String) -> &str {
+        let bytes = s.as_bytes();
+        for (i, &item) in bytes.iter().enumerate() {
+            if item == b' ' {
+                return &s[0..i];
+            }
+        }
+        &s[..]
+    }
+    println!("the first word is: {}", word);
 }
