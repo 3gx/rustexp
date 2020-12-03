0c97a4207a0cdceccd48232df8853ac4261daf07
Evghenii <egx@egx-mbp.local>
2020-12-02 19:04:22 -0800

    update ch10a

--- rust_book/ch10/ch10a/src/main.rs
+++ rust_book/ch10/ch10a/src/main.rs
@@ -1,23 +1,49 @@
-fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
 
-    let mut largest = number_list[0];
-
-    for number in number_list {
+    for number in list {
         if number > largest {
             largest = number
         }
     }
+    largest
+}
 
-    println!("the largest s {}", largest);
+fn main() {
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let mut largest = number_list[0];
+
+        for number in number_list {
+            if number > largest {
+                largest = number;
+            }
+        }
+
+        println!("The largest number is {}", largest);
+
+        let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+        let mut largest = number_list[0];
+
+        for number in number_list {
+            if number > largest {
+                largest = number;
+            }
+        }
+
+        println!("The largest number is {}", largest);
+    }
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+
+    println!("the largest s {}", result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
 
-    let mut largest = number_list[0];
+    let result = largest(&number_list);
 
-    for number in number_list {
-        if number > largest {
-            largest = number
-        }
-    }
+    println!("the largest s {}", result);
 }
