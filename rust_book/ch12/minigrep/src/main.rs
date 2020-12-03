f25ac286451c648ab0e66d7fec9560f7f80fe2e9
Evghenii <egaburov@nvidia.com>
2020-12-03 12:38:45 -0800

    step 1 is completed

--- rust_book/ch12/minigrep/src/main.rs
+++ rust_book/ch12/minigrep/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let querry = &args[1];
+    let filename = &args[2];
+    println!("Searching for '{}'", querry);
+    println!("In file '{}'", filename);
 }
+
