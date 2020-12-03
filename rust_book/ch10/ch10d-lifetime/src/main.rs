07b2b194a81eed967ea3e7f35d89bdfab92a2d98
Evghenii <egx@egx-mbp.local>
2020-12-02 20:41:26 -0800

    update ch10d

--- rust_book/ch10/ch10d-lifetime/src/main.rs
+++ rust_book/ch10/ch10d-lifetime/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+
+    let r;
+
+    {
+        let x = 5;
+        r = &x;
+        println!("r: {}", r);
+    }
+
+//    println!("r: {}", r); // `rustc --explain E0597`
 }
