f7dc59dab61d414f8a6fedd84846387e88907983
Evghenii <egaburov@nvidia.com>
2020-12-03 09:33:03 -0800

    update ch11

--- rust_book/ch11/adder/src/lib.rs
+++ rust_book/ch11/adder/src/lib.rs
@@ -3,5 +3,38 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+        assert!(3 == 4 - 1);
+    }
+
+    #[test]
+    fn crash() {
+        panic!("oops");
+    }
+
+    #[test]
+    #[should_panic(expected = "panic here")]
+    fn must_fail() {
+        panic!("panic here");
+    }
+
+    #[test]
+    #[should_panic]
+    fn exploration() {
+        assert_eq!(2 + 2, 5);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = String::from("Carol");
+        assert!(false, "failed here  @'{}'", result);
+    }
+
+    #[test]
+    fn it_works1() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
