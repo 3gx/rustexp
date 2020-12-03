f2ce2dc2556719c3c661e1cbbb9501db26e12b8f
Evghenii <egx@egx-mbp.local>
2020-12-02 19:18:31 -0800

    update ch10b

--- rust_book/ch10/ch10b/src/main.rs
+++ rust_book/ch10/ch10b/src/main.rs
@@ -22,14 +22,51 @@ fn largest_char(list: &[char]) -> &char {
     largest
 }
 
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+#[derive(Debug)]
+struct Point<T> {
+    x : T,
+    y : T,
+}
+
+#[derive(Debug)]
+struct Point1<T, U> {
+    x : T,
+    y : U,
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
     let result = largest_char(&char_list);
     println!("The largest char is {}", result);
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+//    let mixed = Point { x: 1, y: 4.0 };  // fails
+    println!("integer= {:?}", integer);
+    println!("integer= {:#?}", float);
+
+    let mixed = Point1 { x: 1, y: 4.0 };
+    println!("integer= {:?}", mixed);
 }
