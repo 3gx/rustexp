4e0299ef8b15d1e4f848b162a5021e235e489f5e
Evghenii <egaburov@nvidia.com>
2020-12-05 11:55:29 -0800

    update refc

--- rust_book/ch15/refc/src/main.rs
+++ rust_book/ch15/refc/src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after create a = {}", Rc::strong_count(&a));
+    println!("weak_count after create a = {}", Rc::weak_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after create b = {}", Rc::strong_count(&a));
+    println!("weak_count after create b = {}", Rc::weak_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("c= {:?}", c);
+        println!("count after create c = {}", Rc::strong_count(&a));
+    println!("weak_count after create c = {}", Rc::weak_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    println!("weak_count after c goes out of scope = {}", Rc::weak_count(&a));
+    println!("a= {:?}", a);
+    println!("b= {:?}", b);
 }
