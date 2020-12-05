16fab357e795442bf542ef689762041b2ceeabba
Evghenii <egaburov@nvidia.com>
2020-12-05 11:34:55 -0800

    update mybox

--- rust_book/ch15/mybox/src/main.rs
+++ rust_book/ch15/mybox/src/main.rs
@@ -1,3 +1,39 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y = &x;
+    let z = Box::new(x);
+    let w = MyBox::new(x);
+
+    assert_eq!(5,x);
+    assert_eq!(5,*y);
+    assert_eq!(5,*z);
+    assert_eq!(5,w.0);
+    assert_eq!(5,*w);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    let m = MyBox::new(String::from("Rusty"));
+    hello(&(*m)[..]);
 }
