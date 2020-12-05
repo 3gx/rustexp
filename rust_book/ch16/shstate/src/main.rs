9f3b4727078a49dc0a4efa9782a20252451b9239
Evghenii <egx@egx-mbp.local>
2020-12-05 15:17:42 -0800

    update shstate

--- rust_book/ch16/shstate/src/main.rs
+++ rust_book/ch16/shstate/src/main.rs
@@ -1,3 +1,40 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let m = Mutex::new(5);
+
+        {
+            {
+                let mut _num = m.lock().unwrap();
+            }
+            let mut num = m.lock().unwrap();
+            *num = 6;
+        }
+        println!("m= {:?}", m);
+    }
+
+    {
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for i in 0..10 {
+            println!("i= {}", i);
+        }
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        println!("Result: {}", counter.lock().unwrap());
+    }
 }
