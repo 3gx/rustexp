346a2156ef1a6606ae8f9c529629e5364dfcdb42
Evghenii <egaburov@nvidia.com>
2020-12-05 08:05:48 -0800

    update ch13

--- rust_book/ch13/src/main.rs
+++ rust_book/ch13/src/main.rs
@@ -3,19 +3,21 @@ use std::time::Duration;
 
 fn simulated_expensive_calc(intensity: u32) -> u32 {
     println!("calculating slowly..");
-//    thread::sleep(Duration::from_secs(2));
+    //    thread::sleep(Duration::from_secs(2));
     intensity
 }
 
 struct Cacher<T>
-where T: Fn(u32) -> u32
+where
+    T: Fn(u32) -> u32,
 {
     calculation: T,
     value: Option<u32>,
 }
 
 impl<T> Cacher<T>
-where T: Fn(u32) -> u32,
+where
+    T: Fn(u32) -> u32,
 {
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
@@ -67,10 +69,9 @@ fn main() {
     let y = 4;
     assert!(equal_to_x(y));
 
-    let x = vec![1,2,3];
+    let x = vec![1, 2, 3];
     let equal_to_x = move |z| z == x;
-//    println!("can't use x here: {:?}", x);
-    let y = vec![1,2,3];
+    //    println!("can't use x here: {:?}", x);
+    let y = vec![1, 2, 3];
     assert!(equal_to_x(y));
-
 }
