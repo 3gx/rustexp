dfc2f410f90cd85fd93f868b4328d2e0825608e9
Evghenii <egaburov@nvidia.com>
2020-12-05 11:40:14 -0800

    update drop

--- rust_book/ch15/drop/src/main.rs
+++ rust_book/ch15/drop/src/main.rs
@@ -1,3 +1,24 @@
+struct CSP {
+    data: String
+}
+
+impl Drop for CSP {
+    fn drop(&mut self) {
+        println!("Dropping CSP with data `{}`!", self.data);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let _c = CSP {
+        data: String::from("my stuff"),
+    };
+    {
+        let _d = CSP {
+            data: String::from("other stuff"),
+        };
+    }
+    let _e = CSP {
+        data: String::from("my stuff e"),
+    };
+    drop(_c);
+    println!("CustomSmartPointers created.");
 }
