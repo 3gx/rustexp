6f2e384d5bc5e94b49e2c0cd69bf5c244cfe0482
Evghenii <egx@egx-mbp.local>
2020-12-05 16:27:09 -0800

    update traits

--- rust_book/ch17/traits/src/lib.rs
+++ rust_book/ch17/traits/src/lib.rs
@@ -1,3 +1,44 @@
 pub trait Draw {
     fn draw(&self);
 }
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Screen1<T: Draw> {
+    pub components: Vec<T>,
+}
+
+impl<T> Screen1<T>
+where
+    T: Draw,
+{
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("draw button");
+    }
+}
--- rust_book/ch17/traits/src/main.rs
+++ rust_book/ch17/traits/src/main.rs
@@ -1,3 +1,50 @@
+#[derive(Debug)]
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+use traits::{Button, Draw, Screen};
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("draw selectBox");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    {
+        let screen = Screen {
+            components: vec![
+                Box::new(SelectBox {
+                    width: 75,
+                    height: 10,
+                    options: vec![
+                        String::from("Yes"),
+                        String::from("Maybe"),
+                        String::from("No"),
+                    ],
+                }),
+                Box::new(Button {
+                    width: 50,
+                    height: 10,
+                    label: String::from("OK"),
+                }),
+            ],
+        };
+
+        screen.run();
+    }
+
+    /*
+     {
+         let screen = Screen {
+             components: vec![Box::new(String::from("Hi"))],
+    //                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ the trait `Draw` is not implemented
+         };
+
+         screen.run();
+     } // `rustc --explain E0277`.
+     */
 }
