861e68b92e8b7e41f3d53bbf27f4ded3b62f7aeb
Evghenii <egaburov@nvidia.com>
2020-12-06 09:34:23 -0800

    update types

--- rust_book/ch19/types/src/main.rs
+++ rust_book/ch19/types/src/main.rs
@@ -1,3 +1,28 @@
+type Km = i32;
+type Thunk = Box<dyn Fn() + Send + 'static>;
+
+fn takes_long_type(f: Thunk) {
+    f()
+}
+
+fn returns_long_type() -> Thunk {
+    Box::new(|| println!("hi"))
+}
+
+fn _bar() -> ! {
+    panic!("oops") // type-checks
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 5;
+    let y: Km = 10;
+    println!("x+y={}", x + y);
+
+    let f: Thunk = returns_long_type();
+    takes_long_type(f);
+
+    // _bar(); // panics
+
+    // let _s1 : str = "Hello there!";     // FAIL, size can't be known at compile time
+    let _s2: &str = "How's it going"; // OK
 }
