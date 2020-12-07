d5e2f52748222a9db9de8f699aa4d6bbc3feea7b
Evghenii <egaburov@nvidia.com>
2020-12-07 11:05:49 -0800

    impl iterator trait

--- linkedlists/src/second.rs
+++ linkedlists/src/second.rs
@@ -7,6 +7,22 @@ pub struct List<T> {
 
 type Link<T> = Option<Box<Node<T>>>;
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+
 #[derive(Debug)]
 struct Node<T> {
     elem: T,
@@ -96,5 +112,19 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
