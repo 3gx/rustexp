f32b8eae0c3d93aa8392753f36a8022b4568a065
Evghenii <egaburov@nvidia.com>
2020-12-07 10:12:41 -0800

    update first

--- linkedlists/src/first.rs
+++ linkedlists/src/first.rs
@@ -1,18 +1,23 @@
+use std::mem;
+
 #[derive(Debug)]
 pub enum List0<T> {
     Cons(T, Box<List0<T>>),
     Nil,
 }
 
+#[derive(Debug)]
 pub struct List {
     head: Link,
 }
 
+#[derive(Debug)]
 enum Link {
     Empty,
     More(Box<Node>),
 }
 
+#[derive(Debug)]
 struct Node {
     elem: i32,
     next: Link,
@@ -21,5 +26,52 @@ struct Node {
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // check empty
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
