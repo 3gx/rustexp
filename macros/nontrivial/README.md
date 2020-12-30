bd1f34d29a8e7d287bd68f4e05a4df8b48bb75d6
Evghenii <egx@egx-mbp.local>
2020-12-30 15:10:41 -0800

    add

---
+++ macros/nontrivial/README.md
@@ -0,0 +1,1 @@
+https://adventures.michaelfbryan.com/posts/non-trivial-macros/
--- macros/nontrivial/src/main.rs
+++ macros/nontrivial/src/main.rs
@@ -1,3 +1,9 @@
+macro_rules! visit_members {
+    ( fn $name:ident(&self $(, $arg_name:ident : $arg_ty:ty)*) $(-> $ret:ty)?; ) => {}
+}
 fn main() {
-    println!("Hello, world!");
+    visit_members! { fn get_x(&self) -> u32; }
+    visit_members! { fn get_x(&self, x : i32) -> u32; }
+    visit_members! { fn get_x(&self, x : i32, y : impl X()) -> u32; }
+    visit_members! { fn get_x(&self, x : i32); }
 }
