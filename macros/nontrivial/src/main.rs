2a182ad47a0a69fec1111d6a82cca57c3a2640c9
Evghenii <egx@egx-mbp.local>
2020-12-30 15:27:44 -0800

    checkpoint

--- macros/nontrivial/src/main.rs
+++ macros/nontrivial/src/main.rs
@@ -1,7 +1,30 @@
 macro_rules! visit_members {
-    ( $( #[$attr:meta] )*
-      fn $name:ident(&self $(, $arg_name:ident : $arg_ty:ty)*) $(-> $ret:ty)?; ) => {};
+    (
+      $( #[$attr:meta] )*
+      fn $name:ident(&self $(, $arg_name:ident : $arg_ty:ty)*) $(-> $ret:ty)?;
+      $ ( $rest:tt )*
+      ) => {};
+
+    (
+        $callback:ident;
+        $( #[$attr:meta] )*
+        fn $name:ident(&self $(, $arg_name:ident : $arg_ty:ty)*) $(-> $ret:ty)?;
+        $( $rest:tt )*
+      ) => {
+        $callback!(
+            $( #[$attr] )*
+            fn $name(&self, $(, $arg_name : $arg_ty)*) $(-> $ret)?
+        );
+        visit_members![ $callback; $($rest)* ];
+      };
+    ($callback:ident;) => {};
+    () => {}
 }
+/*
+macro_rules! my_callback {
+    ( $($whatever:tt)* ) => {}
+}
+*/
 fn main() {
     visit_members! { fn get_x(&self) -> u32; }
     visit_members! { fn get_x(&self, x : i32) -> u32; }
@@ -12,5 +35,12 @@ fn main() {
     visit_members![
         #[disallow]
         fn get_x(&self, x: i32) -> i32;
+        #[derive(Debug)]
+        fn get_y(&self, x: i32) -> i32;
     ];
+    visit_members!{
+        stringify;
+        #[disallow]
+        fn get_x(&self, x: i32) -> i32;
+    };
 }
