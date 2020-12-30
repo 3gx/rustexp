7be54f452b9ffb86c9c5ea230fc9cd7c06a62a34
Evghenii <egx@egx-mbp.local>
2020-12-30 15:13:30 -0800

    wip

--- macros/nontrivial/src/main.rs
+++ macros/nontrivial/src/main.rs
@@ -1,9 +1,16 @@
 macro_rules! visit_members {
-    ( fn $name:ident(&self $(, $arg_name:ident : $arg_ty:ty)*) $(-> $ret:ty)?; ) => {}
+    ( $( #[$attr:meta] )*
+      fn $name:ident(&self $(, $arg_name:ident : $arg_ty:ty)*) $(-> $ret:ty)?; ) => {};
 }
 fn main() {
     visit_members! { fn get_x(&self) -> u32; }
     visit_members! { fn get_x(&self, x : i32) -> u32; }
     visit_members! { fn get_x(&self, x : i32, y : impl X()) -> u32; }
-    visit_members! { fn get_x(&self, x : i32); }
+    visit_members!(
+        fn get_x(&self, x: i32);
+    );
+    visit_members![
+        #[disallow]
+        fn get_x(&self, x: i32) -> i32;
+    ];
 }
