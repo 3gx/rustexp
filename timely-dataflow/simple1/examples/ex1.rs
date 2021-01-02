dc105c390d84200397c047d38fe2c60a9e1ee38f
Evghenii <egx@egx-mbp.local>
2021-01-02 10:32:02 -0800

    add example

---
+++ timely-dataflow/simple1/examples/ex1.rs
@@ -0,0 +1,9 @@
+
+use timely::dataflow::operators::{ToStream, Inspect};
+
+fn main() {
+    timely::example(|scope| {
+        (0..10).to_stream(scope)
+               .inspect(|x| println!("seen: {:?}", x));
+    });
+}
