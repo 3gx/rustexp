204a3ce7a521d8eb1606791e29ed111173b05972
Evghenii <egx@egx-mbp.local>
2021-01-15 06:19:28 -0800

    Add mymatch macro

--- misc/macros/mymatch1/src/main.rs
+++ misc/macros/mymatch1/src/main.rs
@@ -1,3 +1,31 @@
+#![feature(decl_macro)]
+macro mymatch {
+   ( $obj:expr, $($matcher:pat $(if $pred:expr)* => $result:expr),*) => {
+       match $obj {
+           $($matcher $(if $pred)* => $result),*
+       }
+   }
+}
 fn main() {
-    println!("Hello, world!");
+    let matchme = |x| {
+        mymatch! { x,
+            Some(Some(7)) => println!("seven!"),
+            Some(x) => println!("Some(x), x=  {:?}", x),
+            None => println!("None (jawdrop)")
+        }
+    };
+
+    let x = Some(Some(7));
+    matchme(x);
+    matchme(Some(Some(5)));
+    matchme(Some(None));
+    matchme(None);
+
+    let x = 10;
+    let y = mymatch! {
+        x,
+     10 => "Ten",
+     _ => "something else"
+    };
+    println!("y={:?}", y);
 }
