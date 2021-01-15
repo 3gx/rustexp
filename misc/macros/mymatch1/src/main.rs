038bde439c0ca4616054aa0ccb74d40b357dc8f2
Evghenii <egx@egx-mbp.local>
2021-01-15 07:02:54 -0800

    some changes

--- misc/macros/mymatch1/src/main.rs
+++ misc/macros/mymatch1/src/main.rs
@@ -1,14 +1,29 @@
 #![feature(decl_macro)]
 macro mymatch {
+   ( $obj:expr; $($matcher:pat $(if $pred:expr)* => $result:expr),*) => {
+       match $obj {
+           $($matcher $(if $pred)* => $result),*
+       }
+   }
+}
+/*
+macro mymatch1 {
+   ( | $($tail:tt)* ) => { mymatch1!(@s1 $($tail)*) },
+   (@s1 $obj:expr  $($tail:tt)* ) => {
+       match $obj {
+           mymatch1!(@s2 $($tail)*)
+       }
+   },
    ( $obj:expr, $($matcher:pat $(if $pred:expr)* => $result:expr),*) => {
        match $obj {
            $($matcher $(if $pred)* => $result),*
        }
    }
 }
+*/
 fn main() {
     let matchme = |x| {
-        mymatch! { x,
+        mymatch! { x;
             Some(Some(7)) => println!("seven!"),
             Some(x) => println!("Some(x), x=  {:?}", x),
             None => println!("None (jawdrop)")
@@ -23,7 +38,7 @@ fn main() {
 
     let x = 10;
     let y = mymatch! {
-        x,
+        x;
      10 => "Ten",
      _ => "something else"
     };
