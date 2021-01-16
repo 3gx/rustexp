ffce2f2346ea47ba2a5b1a39f8e047744199e771
Evghenii <egx@egx-mbp.local>
2021-01-15 16:52:21 -0800

    added callback stub

--- misc/macros/callback1/src/main.rs
+++ misc/macros/callback1/src/main.rs
@@ -1,5 +1,9 @@
-#[feature(decl_macro)]
+#![feature(decl_macro)]
 use if_chain;
+
+macro mk_chain($($tt:tt)*) {
+    42
+}
 
 fn main() {
     let x1 = Some(20);
@@ -8,6 +12,14 @@ fn main() {
         if let 20 = i;
         then { true }
         else {false}
+    };
+    println!("x={}", x);
+
+    let x = mk_chain! {
+        {if let Some(i) = x1,
+         if let 20 = i },
+        true,
+        false,
     };
     println!("x={}", x);
 }
