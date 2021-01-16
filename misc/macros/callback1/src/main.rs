72d2bc896631db2ef449544a1a414bfd8e175fcb
Evghenii <egx@egx-mbp.local>
2021-01-15 16:49:10 -0800

    checkpoint

--- misc/macros/callback1/src/main.rs
+++ misc/macros/callback1/src/main.rs
@@ -1,3 +1,13 @@
+#[feature(decl_macro)]
+use if_chain;
+
 fn main() {
-    println!("Hello, world!");
+    let x1 = Some(20);
+    let x = if_chain::if_chain! {
+        if let Some(i) = x1;
+        if let 20 = i;
+        then { true }
+        else {false}
+    };
+    println!("x={}", x);
 }
