0c18f2139ca325058dfff4772cec7b5ad553232c
Evghenii <egaburov@nvidia.com>
2021-01-16 10:14:55 -0800

    progress

--- misc/macros/callback1/src/main.rs
+++ misc/macros/callback1/src/main.rs
@@ -1,13 +1,32 @@
 #![feature(decl_macro)]
 use if_chain::if_chain;
 
+/*
 macro mk_chain_impl {
     ($callback:ident $($tt:tt)*) => {
         $callback!{$($tt)*}
     }
 }
+*/
+macro mk_chain_impl {
+    (@callback $cb:ident $($body:tt)*) => {
+        $cb! { $($body)* }
+    },
+    (@accum (then $then:tt else $else:tt) -> ($($body:tt)*)) => {
+        mk_chain_impl!(@callback stringify {$($body)* then $then else $else})
+    },
+    (@accum (let $pat:pat = $expr:expr; $($tail:tt)*) -> ($($body:tt)*)) => {
+        mk_chain_impl!(@accum ($($tail)*) -> ($($body)* if let $pat = $expr;))
+    },
+    (@accum ($expr:expr, $($tail:tt)*) -> ($($body:tt)*)) => {
+        mk_chain_impl!(@accum ($($tail)*) -> ($($body)*; if $expr;))
+    },
+}
 macro mk_chain {
-    ($($tt:tt)*) => {mk_chain_impl!(if_chain $($tt)*)},
+    ($($tt:tt)*) => {
+        mk_chain_impl!(@accum ($($tt)*) -> ())
+    },
+//        mk_chain_impl!(if_chain $($tt)*)},
 }
 
 fn main() {
@@ -21,8 +40,9 @@ fn main() {
     println!("x={}", x);
 
     let x = mk_chain! {
-        if let Some(i) = x1;
-         if let 20 = i;
+        let Some(i) = x1;
+        let 20 = i;
+        then {true} else {false}
     };
     println!("x={}", x);
 }
