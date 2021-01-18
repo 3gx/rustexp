09b09cf3750812f50576a0786e6558b7cd7c4f89
Evghenii <egx@egx-mbp.local>
2021-01-18 13:32:06 -0800

    test

--- essentials-of-compilation/ch2/tests/rvar_anf.rs
+++ essentials-of-compilation/ch2/tests/rvar_anf.rs
@@ -5,11 +5,12 @@
 mod rvar_anf_tests {
     #[test]
     fn t1() {
-        use ch2::rvar_anf_lang::{interp_exp, rco_exp};
+        use ch2::rvar_anf_lang;
+        //        use ch2::rvar_anf_lang::{interp_exp, rco_exp};
         let (p1, v1) = {
             // not the same as
             // use ch2::rvar_lang::*;
-            use ch2::rvar_anf_lang::rvar_lang::*;
+            use rvar_anf_lang::rvar_lang::*;
             let p1 = r#let!([x add!(12, add!(neg!(20), neg!(add!(10,neg!(15)))))]
                     add!(add!(30, neg!(15)), x));
             let v1 = interp_exp(&vec![], &p1);
@@ -17,6 +18,7 @@ mod rvar_anf_tests {
         };
         println!("p1= {:#?} ", p1);
 
+        use rvar_anf_lang::{interp_exp, rco_exp};
         let p1anf = rco_exp(&p1);
         println!("p1= {:#?} ", p1anf);
 
