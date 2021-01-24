c26384d1692a1ae4ce8b0f3c67fbc931493900f5
Evghenii <egx@egx-mbp.local>
2021-01-23 17:53:05 -0800

    some more debug printing

--- essentials-of-compilation/ch2/tests/x86var.rs
+++ essentials-of-compilation/ch2/tests/x86var.rs
@@ -40,11 +40,19 @@ mod x86var_lang {
         println!("eval(x86var)= {}", val_x86var);
         assert_eq!(v1, val_x86var);
 
+        fn print_vec<T: std::fmt::Debug>(list: &Vec<T>) {
+            for el in list {
+                println!("\t{:?}", el);
+            }
+        }
+        print_vec(&x86var.1);
+
         let x86var_lang::Block(vars, _) = &x86var;
         println!("vars= {:?}", vars);
 
         let x86var_home = x86var_lang::assign_homes(&x86var);
         println!("assgned_homes= {:?}", x86var_home);
+        print_vec(&x86var_home.1);
 
         let val_x86var_stack = x86var_lang::interp_block_stack(&x86var_home);
         println!("eval(x86var_home)= {}", val_x86var_stack);
@@ -54,6 +62,7 @@ mod x86var_lang {
         println!("x86var_patched= {:?}", x86var_patched);
         let val_x86var_patched = x86var_lang::interp_block_stack(&x86var_patched);
         assert_eq!(v1, val_x86var_patched);
+        print_vec(&x86var_patched.1);
 
         println!("\n{}", x86var_lang::print_x86(&x86var_patched).as_str());
     }
