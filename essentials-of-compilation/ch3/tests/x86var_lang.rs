a642e2997f2049659a2d9153a3046529cbeab2ca
Evghenii <egx@egx-mbp.local>
2021-02-01 11:06:37 -0800

    fun stufF

--- essentials-of-compilation/ch3/tests/x86var_lang.rs
+++ essentials-of-compilation/ch3/tests/x86var_lang.rs
@@ -66,4 +66,49 @@ mod x86var_lang {
 
         println!("\n{}", x86var_lang::print_x86(&x86var_patched).as_str());
     }
+
+    #[test]
+    fn t1() {
+        use ch3::x86var_lang;
+        use cvar_lang::rvar_anf_lang;
+        use x86var_lang::cvar_lang;
+        use x86var_lang::rvar_lang;
+        let (p1, v1) = {
+            use rvar_lang::*;
+            let p1 = r#let!([x add!(12, add!(neg!(20), neg!(add!(10,neg!(15)))))]
+                    add!(add!(30, neg!(15)), x));
+            let v1 = interp_exp(&vec![], &p1);
+            (p1, v1)
+        };
+        //println!("p1= {:?} ", p1);
+
+        use rvar_anf_lang::{interp_exp, rco_exp};
+        let p1anf = rco_exp(&p1);
+        //println!("p1= {:?} ", p1anf);
+
+        let v1anf = interp_exp(&vec![], &p1anf);
+
+        //println!("v1anf= {}", v1anf);
+        assert_eq!(v1, v1anf);
+
+        let (tail, vars) = cvar_lang::explicate_tail(&p1anf);
+        println!("vars= {:?}", vars);
+        //println!("tail= {:?}", tail);
+
+        let cprog = cvar_lang::CProgram(vars.clone(), vec![("start".to_string(), tail.clone())]);
+
+        let v1clang = cvar_lang::interp_prog(&cprog);
+        //println!("v1clang= {}", v1clang);
+        assert_eq!(v1anf, v1clang);
+
+        use x86var_lang::Block;
+        let x86var = x86var_lang::select_inst_tail(&tail, x86var_lang::Block::with_vars(vars));
+        let Block(_, list) = &x86var;
+        for inst in list {
+            println!("  {:?}", inst);
+        }
+        let val_x86var = x86var_lang::interp_block(&x86var);
+        //println!("eval(x86var)= {}", val_x86var);
+        assert_eq!(v1, val_x86var);
+    }
 }
