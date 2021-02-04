438856d4817962137c59db6c14f21b9bd9b8705b
Evghenii <egx@egx-mbp.local>
2021-02-04 10:22:50 -0800

    add some Gc example

--- essentials-of-compilation/ch4/src/main.rs
+++ essentials-of-compilation/ch4/src/main.rs
@@ -1,3 +1,46 @@
+#![cfg_attr(feature = "nightly", feature(specialization))]
+
+use gc::{Finalize, Gc, GcCell, Trace};
+
 fn main() {
+    {
+        let x = Gc::new(1_u8);
+        let y = Gc::new(Box::new(Gc::new(1_u8)));
+
+        #[derive(Trace, Finalize, Debug, Clone)]
+        struct Foo {
+            a: Gc<u8>,
+            b: u8,
+        }
+
+        let z = Gc::new(Foo { a: x.clone(), b: 1 });
+        println!("x= {:?}", x);
+        println!("y= {:?}", y);
+        println!("z= {:?}", z);
+    }
+
+    {
+        #[derive(Trace, Finalize, Debug)]
+        struct Foo {
+            cyclic: GcCell<Option<Gc<Foo>>>,
+            data: u8,
+        }
+
+        let foo1 = Gc::new(Foo {
+            cyclic: GcCell::new(None),
+            data: 1,
+        });
+        let foo2 = Gc::new(Foo {
+            cyclic: GcCell::new(Some(foo1.clone())),
+            data: 2,
+        });
+        let foo3 = Gc::new(Foo {
+            cyclic: GcCell::new(Some(foo2.clone())),
+            data: 3,
+        });
+        println!("foo3= {:?}", foo3);
+        println!("foo1= {:?}", foo1);
+        *foo1.cyclic.borrow_mut() = Some(foo3.clone());
+    }
     println!("Hello, world!");
 }
