c0986a9b44ca5c2bc93891bc5d8273477a00d31e
Evghenii <egx@egx-mbp.local>
2021-02-08 20:19:55 -0800

    fun stuff

--- essentials-of-compilation/ch4/src/cvar_lang.rs
+++ essentials-of-compilation/ch4/src/cvar_lang.rs
@@ -140,9 +140,8 @@ fn explicate_if(
             explicate_if(p_expr, &then_name, &else_name, bbs)
         }
         RVarAnf::Expr::Let(x, expr, body) => {
-            let (then_tail, bbs) = explicate_if(body, then_name, else_name, bbs);
-            let (tail, bbs) = explicate_assign(expr, x, then_tail, bbs);
-            unimplemented!()
+            let (if_tail, bbs) = explicate_if(body, then_name, else_name, bbs);
+            explicate_assign(expr, x, if_tail, bbs)
         }
         x @ _ => panic!("invalid if predicate= {:?}", x),
     }
