364c4f3c9c3d93fd89f948eff17a43843ea7bc52
Evghenii <egaburov@nvidia.com>
2021-02-09 19:40:39 -0800

    some tests

--- essentials-of-compilation/ch4/src/main.rs
+++ essentials-of-compilation/ch4/src/main.rs
@@ -115,4 +115,55 @@ fn main() {
         assert_eq!(graph.vertex_count(), 1);
         assert_eq!(graph.edge_count(), 0);
     }
+
+    {
+        use petgraph::algo::dijkstra;
+        use petgraph::prelude::*;
+        use petgraph::stable_graph::StableGraph as Graph;
+        use std::collections::HashMap;
+
+        let mut graph: Graph<(), (), Directed> = Graph::new();
+        let a = graph.add_node(()); // node with no weight
+        let b = graph.add_node(());
+        let c = graph.add_node(());
+        let d = graph.add_node(());
+        let e = graph.add_node(());
+        let f = graph.add_node(());
+        let g = graph.add_node(());
+        let h = graph.add_node(());
+        // z will be in another connected component
+        let _z = graph.add_node(());
+
+        graph.extend_with_edges(&[
+            (a, b),
+            (b, c),
+            (c, d),
+            (d, a),
+            (e, f),
+            (b, e),
+            (f, g),
+            (g, h),
+            (h, e),
+        ]);
+        // a ----> b ----> e ----> f
+        // ^       |       ^       |
+        // |       v       |       v
+        // d <---- c       h <---- g
+
+        let expected_res: HashMap<NodeIndex, usize> = [
+            (a, 3),
+            (b, 0),
+            (c, 1),
+            (d, 2),
+            (e, 1),
+            (f, 2),
+            (g, 3),
+            (h, 4),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+        let res = dijkstra(&graph, b, None, |_| 1);
+        assert_eq!(res, expected_res);
+    }
 }
