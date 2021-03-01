439840bc2d454cfe67a4463c153ff3ccf3e3d59d
Evghenii <egaburov@nvidia.com>
2021-03-01 08:26:11 -0800

    add another versino

---
+++ misc/unsafe_swap_pointers_bugfix.rs
@@ -0,0 +1,19 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+enum E<T> {
+    E0,
+    E1(Box<T>),
+    E2(Box<T>, Box<T>),
+}
+
+#[repr(transparent)]
+struct S(E<S>);
+
+fn common_s(S(e) : &S) {
+  // .. use e
+}
+fn fun_s1(S(e) : &S) {
+    let p_s: &S = unsafe { &(e as *const E<_> as *const S).read() };
+    common_s(p_s);
+}
