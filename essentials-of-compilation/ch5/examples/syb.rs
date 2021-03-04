514a7f23002998fd42e0c2e0d0d91ac1ddb46647
Evghenii <egaburov@nvidia.com>
2021-03-04 15:32:08 -0800

    checjpoint

--- essentials-of-compilation/ch5/examples/syb.rs
+++ essentials-of-compilation/ch5/examples/syb.rs
@@ -1,3 +1,17 @@
+pub struct Company(Vec<Department>);
+pub struct Department(Name, Manager, Vec<SubUnit>);
+pub enum SubUnit {
+    Person(Employee),
+    Department(Box<Department>),
+}
+pub struct Employee(Person, Salary);
+pub struct Person(Name, Address);
+pub struct Salary(f64);
+
+pub type Manager = Employee;
+pub type Name = &'static str;
+pub type Address = &'static str;
+
 fn main() {
-    println!("sym main");
+    println!("syb main");
 }
