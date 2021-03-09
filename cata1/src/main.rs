63d71794d5b268db39292820e656c566dc7b793b
Evghenii <egaburov@nvidia.com>
2021-03-09 14:59:28 -0800

    add not really cata

--- cata1/src/main.rs
+++ cata1/src/main.rs
@@ -21,10 +21,17 @@ pub fn eval_expr(e: &Expr) -> Int {
     }
 }
 
+pub fn not_really_catta(eval: impl Fn(&Expr) -> Int, e: &Expr) -> Int {
+    eval(e)
+}
+
 fn main() {
     use Expr::*;
     let expr = Mul(Add(Value(1).bx(), Value(2).bx()).bx(), Value(3).bx());
     println!("expr= {:?}", expr);
     let val = eval_expr(&expr);
     println!("val= {:?}", val);
+    let val1 = not_really_catta(eval_expr, &expr);
+    println!("val= {:?}", val1);
+    assert_eq!(val, val1);
 }
