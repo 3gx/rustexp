3bc0c06f744b4be7245cbc47e435aa834d10e053
Evghenii <egaburov@nvidia.com>
2021-03-09 14:56:35 -0800

    add simple xpr

--- cata1/src/main.rs
+++ cata1/src/main.rs
@@ -1,3 +1,19 @@
+pub type Int = i64;
+
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Value(Int),
+    Add(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+}
+impl Expr {
+    pub fn bx(self) -> Box<Self> {
+        Box::new(self)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    use Expr::*;
+    let expr = Mul(Add(Value(1).bx(), Value(2).bx()).bx(), Value(3).bx());
+    println!("expr= {:?}", expr);
 }
