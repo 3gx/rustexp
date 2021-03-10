54950c0c118b57fba00f4710d5bd65471489bcf0
Evghenii <egaburov@nvidia.com>
2021-03-10 09:03:06 -0800

    working on dsl syntax

--- essentials-of-compilation/ch6/tests/rvar_lang.rs
+++ essentials-of-compilation/ch6/tests/rvar_lang.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
 mod rvar_lang {
+    use ch6::rvar_lang as RVar;
+
     #[test]
     fn t0() {
-        use ch6::rvar_lang::*;
+        use RVar::*;
         let p1 = expr! {(let [x (add 12 20)] (add 10 x))};
         let tp1 = typed_expr(p1.clone());
         println!("type= {:?}", tp1);
@@ -36,7 +38,7 @@ mod rvar_lang {
 
     #[test]
     fn t1() {
-        use ch6::rvar_lang::*;
+        use RVar::*;
         let e1 = expr! { (let [x 32] (add (let [x 10] x) x)) };
         let e2 = expr! { (let [x1 32] (add (let [x2 10] x2) x1)) };
         println!("e1= {:?}", e1);
@@ -63,11 +65,10 @@ mod rvar_lang {
 
     #[test]
     fn t2() {
-        use ch6::rvar_lang;
         let (p1, v1) = {
             // not the same as
             // use ch2::rvar_lang::*;
-            use rvar_lang::*;
+            use RVar::*;
             let p = expr! {
                 (let [x (add 12 (add (neg 20) (neg (add 10 (neg 15)))))]
                      (add (add 30 (neg 15)) x))
@@ -81,7 +82,7 @@ mod rvar_lang {
 
     #[test]
     fn t3() {
-        use ch6::rvar_lang::*;
+        use RVar::*;
         let expr = expr! {
             (let [x 0]
                  (let [y 100]
@@ -119,7 +120,7 @@ mod rvar_lang {
     #[test]
     fn t4() {
         let (e, v) = {
-            use ch6::rvar_lang::*;
+            use RVar::*;
             let e = expr! {
             (let [t1 (tuple 3 7)]
                  (let [t2 t1]
@@ -139,7 +140,7 @@ mod rvar_lang {
     #[test]
     fn t5() {
         let (e, v) = {
-            use ch6::rvar_lang::*;
+            use RVar::*;
             let e = expr! {
                 (let [v (tuple (tuple 44))]
                      (let [x (let [w (tuple 48)]
@@ -161,7 +162,7 @@ mod rvar_lang {
     #[test]
     fn t6() {
         let (e, v) = {
-            use ch6::rvar_lang::*;
+            use RVar::*;
             let x = 40;
             let two = 2;
             let e = expr! {
@@ -186,7 +187,7 @@ mod rvar_lang {
     #[test]
     fn t7() {
         let (e, v) = {
-            use ch6::rvar_lang::*;
+            use RVar::*;
             let e = expr! {
                 (tupleref (tupleref (tuple (tuple 42)) 0) 0)
             };
@@ -200,5 +201,27 @@ mod rvar_lang {
         };
         println!("e= {:?}", e);
         println!("v= {:?}", v);
+    }
+
+    #[test]
+    fn t06a() {
+        let (p, v) = {
+            /*
+            use RVar::*;
+            let p = program! {
+                (fundef (map_tu2 [f: (Int -> Int)]
+                                 [v: (Tuple Int Int)]) : (Tuple Int Int)
+                        (tuple (f (tupleref v 0) (tupleref v 1))))
+                (fundef (add1 [x : Int]) : Int
+                        (add x 1))
+                (tupleref (map_tu2 add1 (vector 0 41)) 1)
+            };
+            */
+            let p = 41;
+            let v = 42;
+            (p, v)
+        };
+        println!("prog= {:?}", p);
+        println!("pval= {:?}", v);
     }
 }
