274b6372ca5b6e0c3aaa997ef81d46d8d0aa2b9c
egx <egx>
2021-04-18 09:16:17 -0700

    fix test

--- simpler_easier_dtlc/src/ulc.rs
+++ simpler_easier_dtlc/src/ulc.rs
@@ -28,12 +28,12 @@ mod test {
     fn test1() {
         use ExprK::*;
         let expr: Expr = App(
-            Lam("x".into(), Lam("y".into(), Var("y".into()).into()).into()).into(),
+            Lam("x".into(), Lam("y".into(), Var("x".into()).into()).into()).into(),
             Lam("z".into(), Var("z".into()).into()).into(),
         )
         .into();
         let fmt = format!("{:?}", expr);
-        assert_eq!(fmt, "Expr(App(Expr(Lam(\"x\", Expr(Lam(\"y\", Expr(Var(\"y\")))))), Expr(Lam(\"z\", Expr(Var(\"z\"))))))");
+        assert_eq!(fmt, "Expr(App(Expr(Lam(\"x\", Expr(Lam(\"y\", Expr(Var(\"x\")))))), Expr(Lam(\"z\", Expr(Var(\"z\"))))))");
         println!("expr= {}", fmt);
     }
 }
