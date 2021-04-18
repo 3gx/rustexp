1486b7292a8a6f9b548634fb524d43bd6b344dce
egx <egx>
2021-04-18 09:11:37 -0700

    add test

--- simpler_easier_dtlc/src/ulc.rs
+++ simpler_easier_dtlc/src/ulc.rs
@@ -36,6 +36,8 @@ mod test {
             Lam("z".to_string(), Var("z".to_string()).into()).into(),
         )
         .into();
-        println!("expr= {:?}", expr);
+        let fmt = format!("{:?}", expr);
+        assert_eq!(fmt, "Expr(App(Expr(Lam(\"x\", Expr(Lam(\"y\", Expr(Var(\"y\")))))), Expr(Lam(\"z\", Expr(Var(\"z\"))))))");
+        println!("expr= {}", fmt);
     }
 }
